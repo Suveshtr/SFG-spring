d4d905c8afafdaf74df85e05ac06c3fbd5811431
Suveshtr <suvesh.trivedi@gmail.com>
2019-04-12 22:08:35 +0530

    Update Map Impl to mange setting of ID property. Close#23.

--- pet-clinic-data/src/main/java/guru/springframework/sfgspring/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspring/services/map/AbstractMapService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgspring.services.map;
 
+import guru.springframework.sfgspring.model.BaseEntity;
+
 import java.util.*;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -13,8 +15,16 @@ public abstract class AbstractMapService<T, ID> {
         return Optional.of(map.get(id));
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -24,5 +34,11 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(ele -> ele.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1;
+
+        return nextId;
     }
 }
