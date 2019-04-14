9586250d7430696d6f5648f1a1c9af5012fb7fc8
Suveshtr <suvesh.trivedi@gmail.com>
2019-04-14 19:30:08 +0530

    Menu links are borken. Close#45.

--- pet-clinic-web/src/main/java/guru/springframework/sfgspring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgspring/controllers/OwnerController.java
@@ -21,4 +21,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping({"/find", ""})
+    String findOwners() {
+        return "notimplemented";
+    }
 }
