5080d3f406fe98b65322a9818f1334f0d29ebfba
Suveshtr <suvesh.trivedi@gmail.com>
2019-04-24 19:47:38 +0530

    Add Spring Data JPA service for Pet Types. Close #42.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgspring/services/springdatajpa/PetTypeJpaService.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgspring.services.springdatajpa;
+
+import guru.springframework.sfgspring.model.PetType;
+import guru.springframework.sfgspring.repositories.PetTypeRepository;
+import guru.springframework.sfgspring.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public Optional<PetType> findById(Long aLong) {
+        return petTypeRepository.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
